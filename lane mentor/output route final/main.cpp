bdea84593628f2b723f728a3f61df45bba35ea37
yujifan <yujifan0326@163.com>
2017-02-27 18:00:42 +0800

    lane mentor for hang driver with floyd
    containing a series of projects and files,besides this includes the
    design picture of model area

---
+++ lane mentor/output route final/main.cpp
@@ -0,0 +1,45 @@
+/*
+ * 这是最终打表得出的函数
+ * designed by 于济凡
+ * 2017.2.27
+ */
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int next_Node[50][50]={0};
+
+ifstream fin("/Users/yujifan/Desktop/next node info.txt");
+
+void init()
+{
+    int x,y,next;
+    for (int i = 0; i < 753; ++i) {
+        fin>>x>>y>>next;
+        next_Node[x][y]=next;
+    }
+}
+
+void output(int start, int final)
+{
+    int x,y;
+    if(next_Node[start][final]!=0)
+    {
+        cout<<next_Node[start][final]<<endl;
+    }
+    else
+    {
+        printf("No existed access!");
+    }
+}
+
+int main()
+{
+    int x,y;
+    init();
+    while (cin>>x>>y)
+    {
+        output(x,y);
+    }
+    return 0;
+}
