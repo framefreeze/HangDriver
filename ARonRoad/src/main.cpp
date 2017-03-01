f3cf1b75ae6f86132c8697650dcc8d4e9c92c675
Kevin_Feng <Kevin_Feng@139.com>
2017-03-01 22:45:50 +0800

    AR_code v1.5 fasten AR graph

--- ARonRoad/src/main.cpp
+++ ARonRoad/src/main.cpp
@@ -6,16 +6,10 @@
 #include <glu.h>
 #include <opencv2/opencv.hpp>
 #include <cstring>
+#include "GeometryTypes.hpp"
+#include "CameraCalibration.hpp"
 using namespace std;
 int main(int argc, char ** argv)
 {
-    string windowName = "hello";
-    int windowHeight = 720;
-    int windowWidth = 1280;
-    cv::namedWindow(windowName, cv::WINDOW_OPENGL);
-    // Resize it exactly to video size
-    cv::resizeWindow(windowName, windowWidth, windowHeight);
-    // Initialize OpenGL draw callback:
-    cv::setOpenGlContext(windowName);
-    cv::setOpenGlDrawCallback(windowName, ARDrawingContextDrawCallback, this);
+
 }
