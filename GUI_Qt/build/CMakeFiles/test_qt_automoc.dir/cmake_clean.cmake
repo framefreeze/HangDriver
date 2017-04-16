7f1b54dd5f32853c3d08d29c692f6c6a1c21bce1
Haoqiang Wang <davidwhq123@gmail.com>
2017-04-16 14:22:40 +0800

    fix ignore and delete build cashe

--- GUI_Qt/build/CMakeFiles/test_qt_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "test_qt_automoc.cpp"
-  "CMakeFiles/test_qt_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/test_qt_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
